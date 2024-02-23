ffec99d9a38c20b85a5892baee85e62b5687df98
noecoornt <noe.cornet14@gmail.com>
2024-02-23 18:32:53 +0100

    deuxième commit Noé CORNET

--- calculer_inclinaison_point.py
+++ calculer_inclinaison_point.py
@@ -1,8 +1,16 @@
+import math
 def calculer_inclinaison_point(point_1,angle_1,direction):
+    angle_1=angle_1*(math.pi/180)
+    m = float(math.tan(angle_1))
     if direction == 'x':
+        n_x = round(point_1[0]+m*point_1[1],2)
+        n_y = float(point_1[1])
 
+    elif direction == 'y':
+        n_x =  point_1[0]
+        n_y =  round(point_1[0]*m+point_1[2],2)
+    return n_x,n_y
 
+print(calculer_inclinaison_point((2,4),5,'x'))
 
-
-    return tuple_1
 gg
---
+++ coordonnees_clou.py
@@ -0,0 +1,3 @@
+def calculer_coordonnes_clous(A,B,C,D,E):
+    pt_0=(-B/2),
+    return
